75b9a4ad1bb980962890b106c0d7bdff4d440b92
Tham <zarub2k@gmail.com>
2019-01-05 18:34:06 +0530

    Image loading work is started

--- lib/layouts/row.dart
+++ lib/layouts/row.dart
@@ -19,6 +19,28 @@ class FlutterBarRow extends StatelessWidget {
   }
 
   _buildBody() {
+    return Row(
+      children: _buildChildren(),
+    );
+  }
 
+  _buildChildren() {
+    List<String> images = new List();
+    images.add("01.jpg");
+    images.add("02.jpg");
+    images.add("03.jpg");
+    images.add("04.jpg");
+    images.add("05.jpg");
+
+    List<Widget> children = new List<Widget>();
+    images.forEach((name) => children.add(_buildChild(name)));
+
+    return children;
+  }
+
+  Container _buildChild(String name) {
+    return Container(
+      child: Image.asset('assets/images/$name')
+    );
   }
 }
