2c42eae4c17265cca8a8310a150b20431ebbbbc0
Tham <zarub2k@gmail.com>
2019-01-05 16:07:46 +0530

    Transformed container work done

--- lib/layouts/container.dart
+++ lib/layouts/container.dart
@@ -25,8 +25,38 @@ class FlutterBarContainer extends StatelessWidget {
       children: <Widget>[
         _buildStyledBox(),
         SizedBox(height: 5.0,),
-        _buildCircledBox()
+        _buildCircledBox(),
+        SizedBox(height: 5.0,),
+        _buildImageBox(),
+        SizedBox(height: 5.0,),
+        _buildTransformedBox()
       ],
+    );
+  }
+
+  _buildTransformedBox() {
+    return Center(
+      child: Container(
+        child: Text('Image container'),
+        padding: EdgeInsets.all(15.0),
+        color: Colors.blue,
+        transform: Matrix4.rotationZ(0.5),
+      ),
+    );
+  }
+
+  _buildImageBox() {
+    return Center(
+      child: Container(
+        child: Text('Image container'),
+        padding: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://image.freepik.com/free-vector/spot-light-background_1284-4685.jpg'),
+            centerSlice: Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0),
+          ),
+        )
+      ),
     );
   }
 
@@ -34,7 +64,6 @@ class FlutterBarContainer extends StatelessWidget {
     return Center(
       child: Container(
         child: Text('Circular container'),
-//        color: Colors.amber,
         padding: EdgeInsets.all(50.0),
         decoration: BoxDecoration(
           color: Colors.amber,
@@ -48,7 +77,6 @@ class FlutterBarContainer extends StatelessWidget {
     return Center(
       child: Container(
         child: Text('Styled container'),
-//        color: Colors.amber,
         padding: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0)),
