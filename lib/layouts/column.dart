ca12ea2e32084bbcf2ebe6fed0289fa132374e6a
Tham <zarub2k@gmail.com>
2019-01-07 19:56:59 +0530

    column work started

--- lib/layouts/column.dart
+++ lib/layouts/column.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class FlutterBarColumn extends StatelessWidget {
-  FlutterBarColumn({Key key, this.title}): super()
+  FlutterBarColumn({Key key, this.title}): super(key: key);
+  final String title;
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return _buildUi(context);
   }
 
   _buildUi(BuildContext context) {
@@ -13,6 +15,40 @@ class FlutterBarColumn extends StatelessWidget {
         title: Text(title),
       ),
       body: _buildBody(),
+    );
+  }
+
+  _buildBody() {
+    return Column(
+      children: _buildChildren(),
+    );
+  }
+
+  _buildChildren() {
+    List<String> images = new List();
+    images.add("01.jpg");
+    images.add("02.jpg");
+    images.add("03.jpg");
+    images.add("04.jpg");
+    images.add("05.jpg");
+
+    List<Widget> children = new List<Widget>();
+    images.forEach((name) => children.add(_buildChild(name)));
+
+    return children;
+  }
+
+  Widget _buildChild(String name) {
+    return Expanded(
+      flex: (name == "03.jpg" ? 3 : 1),
+      child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 10.0, color: Colors.black38),
+            borderRadius: const BorderRadius.all(const Radius.circular(8.0)),
+          ),
+          margin: const EdgeInsets.all(4.0),
+          child: Image.asset('assets/images/$name')
+      ),
     );
   }
 }
