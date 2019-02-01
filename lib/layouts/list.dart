27ae19557760e624a6b2b2d77366f77780e0de79
Tham <zarub2k@gmail.com>
2019-02-01 22:27:09 +0530

    ritual check in

--- lib/layouts/list.dart
+++ lib/layouts/list.dart
@@ -34,7 +34,7 @@ class FlutterBarList extends StatelessWidget {
       },
       separatorBuilder: (BuildContext context, int index) {
         return Divider(
-          color: Color.fromRGBO(10, 100, 100, 0.75),
+          color: Color.fromRGBO(10, 10, 100, 0.75),
         );
       },
       itemCount: 10
